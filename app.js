26c18a98b7696de927a98e6f15b279a31db19b3e
awais9211 <pakmatr.804@gmail.com>
2026-03-26 12:11:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+const increaseButtonE1 = document.querySelector('.counter__button--increase')
+const decreaseButtonE1 = document.querySelector('.counter__button--decrease')
+const resetButtonE1 = document.querySelector(".counter__reset-button")
+const counterValueE1 = document.querySelector(".counter__value")
+
+resetButtonE1.addEventListener("click", function(){
+    counterValueE1.textContent = 0;
+})
+
+decreaseButtonE1.addEventListener("click", function(){
+    const currentValue = counterValueE1.textContent;
+    const currentValueAsNumber = +currentValue;
+    let newValue = currentValueAsNumber - 1;
+    if(newValue < 0){
+        newValue = 0;
+    }
+    counterValueE1.textContent = newValue;
+});
+
+increaseButtonE1.addEventListener("click", function(){
+    const currentValue = counterValueE1.textContent;
+    const currentValueAsNumber = +currentValue;
+    const newValue = currentValueAsNumber + 1;
+    counterValueE1.textContent = newValue;
+});
+
+document.addEventListener("keydown", function(e){
+    const currentValue = counterValueE1.textContent;
+    const currentValueAsNumber = +currentValue;
+
+    if(e.key === "ArrowUp"){
+        const newValue = currentValueAsNumber + 1;
+        counterValueE1.textContent = newValue;
+    }
+
+    if(e.key === "ArrowDown"){
+        let newValue = currentValueAsNumber - 1;
+        if(newValue < 0){
+            newValue = 0;
+        }
+        counterValueE1.textContent = newValue;
+    }
+});
